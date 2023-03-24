045d59d4d37b19a2826808d60d3d6aae1c856349
Gangadhar C <gangadharganga90@gmail.com>
2023-03-24 13:55:30 +0530

    upto dispenseChocolates

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
 using System.Diagnostics.Metrics;
 using System.Drawing;
+using System.Linq.Expressions;
 using System.Net.NetworkInformation;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
@@ -15,16 +16,16 @@ namespace Chocolate_Machine
     public class Dispenser
     {
         public int totalChocolates = 28;
-        public Dictionary<string, int> chocolatesColors = new Dictionary<string, int>()
-        {
-            { "green", 2 },
-            { "silver", 1 },
-            { "blue",3},
-            { "crimson",4},
-            { "purple",5},
-            { "red",6},
-            { "pink",7}
-        };
+        public Dictionary<string, int> chocolatesColors = new Dictionary<string, int>();
+        public Dispenser() {
+            chocolatesColors.Add("green", 2);
+            chocolatesColors.Add("silver",1);
+            chocolatesColors.Add("blue", 3);
+            chocolatesColors.Add("crimson", 4);
+            chocolatesColors.Add("purple", 5);
+            chocolatesColors.Add("red", 6);
+            chocolatesColors.Add("pink", 7);
+        }
         public void addChocolates(string color, int count)
         {
             chocolatesColors[color] += count;
@@ -32,55 +33,96 @@ namespace Chocolate_Machine
         }
         public void removeChocolateOfColor(string color)
         {
-            chocolatesColors[color]--;
-            totalChocolates--;
+            if (chocolatesColors[color] != 0)
+            {
+                chocolatesColors[color]--;
+                totalChocolates--;
+            }
+            else
+            {
+                throw new Exception("Less Amount of Chocolate Available Cannot Dispense");
+            }
         }
         public int removeChocolates(int count)
         {
-            int temp = count;
-            Dictionary<string, int> result = new Dictionary<string, int>();
-            while (count > 0)
+            if (count >= totalChocolates)
             {
-                foreach (KeyValuePair<string, int> item in result)
+                int temp = count;
+                Dictionary<string, int> result = new Dictionary<string, int>();
+                while (count > 0)
                 {
-                    result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + 1;
-                    this.chocolatesColors[item.Key]--;
-                    totalChocolates--;
-                    count--;
+                    foreach (KeyValuePair<string, int> item in result)
+                    {
+                        result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + 1;
+                        this.chocolatesColors[item.Key]--;
+                        totalChocolates--;
+                        count--;
+                        if (count == 0) break;
+                    }
                     if (count == 0) break;
                 }
-                if (count == 0) break;
+                return temp;
             }
-            return temp;
+            else {
+               throw new Exception("Less Amount of Chocolate Available Cannot Remove");
+                
+            }
         }
         public int dispenseChocolates(int count)
-        {
-            int temp = count;
-            Dictionary<string, int> result = new Dictionary<string, int>();
-            while (count > 0)
+        { 
+            if (count >= totalChocolates)
             {
-                foreach (KeyValuePair<string, int> item in result)
+                int temp = count;
+                Dictionary<string, int> result = new Dictionary<string, int>();
+                while (count > 0)
                 {
-                    result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + 1;
-                    this.chocolatesColors[item.Key]--;
-                    totalChocolates--;
-                    count--;
+                    foreach (KeyValuePair<string, int> item in result)
+                    {
+                        if (chocolatesColors[item.Key] > 0) { 
+                            result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + 1;
+                            chocolatesColors[item.Key]--;
+                            totalChocolates--;
+                            count--;
+                            if (count == 0) break;
+                        }
+                    }
                     if (count == 0) break;
                 }
-                if (count == 0) break;
+                Console.WriteLine($"\nDispensed items are:");
+                foreach (KeyValuePair<string, int> item in result)
+                {
+                    Console.WriteLine($"[{item.Key}, {item.Value}]");
+                }
+                Console.WriteLine();
+                return temp;
             }
-            return temp;
+            else
+            {
+               throw new Exception("Less Amount of Chocolate Available Cannot Dispense");
+            }
         }
-
-        public Dictionary<string, int> dispenseChocolatesOfColor(string color, int count)
+        public Dictionary<string,int> dispenseChocolatesOfColor(string color, int count)
         {
             Dictionary<string, int> result = new Dictionary<string, int>();
-
-            result[color] = count;
-            chocolatesColors[color] -= count;
-            totalChocolates -= count;
-            return result;
-
+            if (count >= chocolatesColors[color])
+            {
+                result[color] = count;
+                chocolatesColors[color] -= count;
+                totalChocolates -= count;
+                
+                Console.WriteLine($"\nDispensed items are:");
+                foreach (KeyValuePair<string, int> item in result)
+                {
+                    Console.WriteLine($"[{item.Key}, {item.Value}]");
+                }
+                Console.WriteLine();
+                return result;
+            }
+            else
+            {
+                throw new Exception("Less Amount of Chocolate Available Cannot Remove");
+            }
+            
         }
         public void noOfChocolates()
         {
@@ -91,26 +133,47 @@ namespace Chocolate_Machine
         }
         public void changeChocolateColor(int count, string color, string finalColor)
         {
-            chocolatesColors[color] -= count;
-            chocolatesColors[finalColor] += count;
+            if (count > chocolatesColors[color])
+            {
+                chocolatesColors[color] -= count;
+                chocolatesColors[finalColor] += count;
+                Console.WriteLine($"{count} of {color} Chocolates are Changes to {finalColor} Chocolates");
+            }
+            else
+            { 
+                throw new Exception($"Not enough {color} Chocolates are available No Changes are made");
+            }
         }
         public Dictionary<string, int> dispenseRainbowChocolates(int count)
         {
-
-            Dictionary<string, int> result = new Dictionary<string, int>();
-            while (count > 0)
+            if (count > totalChocolates)
             {
-                foreach (KeyValuePair<string, int> item in result)
+                Dictionary<string, int> result = new Dictionary<string, int>();
+                while (count > 0)
                 {
-                    result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + (count > 3 ? 3 : count);
-                    chocolatesColors[item.Key] -= (count > 3 ? 3 : count);
-                    totalChocolates -= (count > 3 ? 3 : count);
-                    count -= (count > 3 ? 3 : count);
+                    foreach (KeyValuePair<string, int> item in result)
+                    {
+                        result[item.Key] = (result.TryGetValue(item.Key, out var value) ? value : 0) + (count > 3 ? 3 : count);
+                        chocolatesColors[item.Key] -= (count > 3 ? 3 : count);
+                        totalChocolates -= (count > 3 ? 3 : count);
+                        count -= (count > 3 ? 3 : count);
+                        if (count == 0) break;
+                    }
                     if (count == 0) break;
                 }
-                if (count == 0) break;
+                Console.WriteLine($"\nDispensed items are:");
+                foreach (KeyValuePair<string, int> item in result)
+                {
+                    Console.WriteLine($"[{item.Key}, {item.Value}]");
+                }
+                Console.WriteLine();
+                return result;
             }
-            return result;
+            else
+            {
+                throw new Exception("!!!No enough chocolates!!!");
+            } 
+            
         }
         public List<string> sortChocolateBasedOnCount()
         {
@@ -127,17 +190,20 @@ namespace Chocolate_Machine
 
         static void Main(string[] args)
         {
-            Dispenser dm = new Dispenser();
-            var list = dm.sortChocolateBasedOnCount();
-            Console.WriteLine($"[{string.Join(", ", list)}]");
-            var list1 = dm.dispenseChocolatesOfColor("silver", 2);
-            foreach (KeyValuePair<string, int> item in list1)
+            try
             {
-                Console.WriteLine(item.Key + " " + item.Value);
-            }
+                Dispenser dm = new Dispenser();
+                var list = dm.sortChocolateBasedOnCount();
+                Console.WriteLine($"[{string.Join(", ", list)}]");
+                var list1 =dm.dispenseChocolates(3);
 
-            dm.addChocolates("silver", 3);
-            dm.noOfChocolates();
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
